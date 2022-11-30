957feac6b5222534990cf4fb6b7d7e639b99527e
Silvio <silvio.brancati4@etu.univ-lorraine.fr>
2022-11-30 11:16:39 +0100

    Animation clé

--- src/main/java/Cases/Key.java
+++ src/main/java/Cases/Key.java
@@ -22,8 +22,16 @@ public class Key extends Case{
     public Key(int x, int y) {
         //Initialisation des attributs
         super(false, x, y, Color.yellow);
-        this.initImage(1);
-        this.setImage(Tools.getImageByName("/images/game/objects/key_found"),0);
+        this.initImage(5);
+        try {
+            this.setImage(Tools.getImageByName("/images/game/objects/key/sprite_key0"), 0);
+            this.setImage(Tools.getImageByName("/images/game/objects/key/sprite_key1"), 1);
+            this.setImage(Tools.getImageByName("/images/game/objects/key/sprite_key2"), 2);
+            this.setImage(Tools.getImageByName("/images/game/objects/key/sprite_key3"), 3);
+            this.setImage(Tools.getImageByName("/images/game/objects/key/sprite_key4"), 4);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 
@@ -52,8 +60,14 @@ public class Key extends Case{
     @Override
     public void draw(BufferedImage image) {
         Graphics2D g = (Graphics2D) image.getGraphics();
-        if (image != null) {
-            g.drawImage(this.getImage(this.getFrame()), this.getY()*TAILLE_CASE, this.getX()*TAILLE_CASE + DrawingPanel.ECART, TAILLE_CASE, TAILLE_CASE, null);
+        this.setTick(this.getTick() + 1);
+        if (this.getTick() >= this.getFrameSpeed()) {
+            this.setTick(0);
+            this.setFrame(this.getFrame() + 1);
+            if (this.getFrame() >= 5) {
+                this.setFrame(0);
+            }
         }
+        g.drawImage(this.getImage(this.getFrame()), this.getY() * TAILLE_CASE, this.getX() * TAILLE_CASE + DrawingPanel.ECART, TAILLE_CASE, TAILLE_CASE, null);
     }
 }
